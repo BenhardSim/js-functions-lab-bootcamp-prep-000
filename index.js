a05ea2017422c1faaaf71da7dc3cb7a7d74bb704
BenhardSIM <benhard.simanullang.bs@gmail.com>
2018-05-02 11:44:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+
+
+Function happyHolidays(){
+return "Happy holidays!"
+}
+Function happyHolidaysto(name){
+  return `Happy holidays, ${name}!`
+}
+Function happyHolidayTo(holiday , name){
+  return `Happy ${holiday}, ${name}!`
+}
+Function holidayCountdown(days , holidays){
+  return `It's ${days} days until ${holiday}!`
+}
+
+happyHolidays();
+happyHolidayTo("you");
+happyHolidaysto("Independence Day" ,  "you");
+holidayCountdown(20 , "Independence Day" );
