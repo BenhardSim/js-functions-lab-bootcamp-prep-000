3ef543219049d74aae99adfcf408527ed8f4b4a5
BenhardSIM <benhard.simanullang.bs@gmail.com>
2018-05-02 11:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysto(name){
 function happyHolidayTo(holiday , name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days , holidays){
+function holidayCountdown(days , holiday){
   return `It's ${days} days until ${holiday}!`
 }
 
